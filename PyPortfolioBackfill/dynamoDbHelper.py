4ab85e67261c550ccd857a3e6b337b2f2d0597e1
rustemvakhitov <rustemvakhitov@hotmail.com>
2025-06-11 13:48:00 -0600

    assigned appropriate picturest to the projects

--- PyPortfolioBackfill/dynamoDbHelper.py
+++ PyPortfolioBackfill/dynamoDbHelper.py
@@ -9,13 +9,14 @@ class DynamoDbHelper():
         self.Config: config.Config = cgf
 
     def loadTags(self, tags):
+        #drop-create table
+        self.dropTable(self.Config.TagsTableName)
         self.createTagsTable()
 
         dynamodb = boto3.resource('dynamodb', region_name=self.Config.Region)
         table = dynamodb.Table(self.Config.TagsTableName)
 
         for key in tags:
-            print(key, tags[key])
             table.put_item(
                 Item={
                     'key': key,
@@ -23,6 +24,34 @@ class DynamoDbHelper():
                 }
             )
 
+        return
+    
+    def loadProjects(self, projects:list[project.Project]):
+        #drop-create table
+        self.dropTable(self.Config.ProjectsTableName)
+        self.createProjectsTable()
+
+        dynamodb = boto3.resource('dynamodb', region_name=self.Config.Region)
+        table = dynamodb.Table(self.Config.ProjectsTableName)
+
+        for project in projects:
+            table.put_item(
+                Item={
+                    'id': project.id,
+                    'key': project.key,
+                    'info': project.toJSON(),
+                }
+            )
+
+        return
+    
+    def dropTable(self, tableName): #self.Config.TagsTableName
+        client = boto3.client('dynamodb', region_name=self.Config.Region)
+
+        if self.tableExists(tableName):
+            client.delete_table(TableName=tableName)
+            waiter = client.get_waiter('table_not_exists')
+            waiter.wait(TableName=tableName)
 
         return
     
@@ -41,12 +70,6 @@ class DynamoDbHelper():
     def createTagsTable(self):
 
         dynamodb = boto3.resource('dynamodb', region_name=self.Config.Region)
-        client = boto3.client('dynamodb', region_name=self.Config.Region)
-
-        if self.tableExists(self.Config.TagsTableName):
-            client.delete_table(TableName=self.Config.TagsTableName)
-            waiter = client.get_waiter('table_not_exists')
-            waiter.wait(TableName=self.Config.TagsTableName)
 
         table = dynamodb.create_table(
         TableName=self.Config.TagsTableName,
@@ -57,6 +80,45 @@ class DynamoDbHelper():
             }
         ],
         AttributeDefinitions=[
+            {
+                'AttributeName': 'key',
+                'AttributeType': 'S'
+            }
+
+        ],
+        ProvisionedThroughput={
+            'ReadCapacityUnits': 10,
+            'WriteCapacityUnits': 10
+        }
+        )
+
+        table.wait_until_exists()
+
+        print("Table status:", table.table_status)
+
+        return
+    
+    def createProjectsTable(self):
+
+        dynamodb = boto3.resource('dynamodb', region_name=self.Config.Region)
+
+        table = dynamodb.create_table(
+        TableName=self.Config.ProjectsTableName,
+        KeySchema=[
+            {
+                'AttributeName': 'id',
+                'KeyType': 'HASH'  #Partition_key
+            },
+            {
+                'AttributeName': 'key',
+                'KeyType': 'RANGE'  #Sort_key
+            }
+        ],
+        AttributeDefinitions=[
+            {
+                'AttributeName': 'id',
+                'AttributeType': 'N'
+            },
             {
                 'AttributeName': 'key',
                 'AttributeType': 'S'
--- PyPortfolioBackfill/program.py
+++ PyPortfolioBackfill/program.py
@@ -7,21 +7,11 @@ import dynamoDbHelper
 
 
 def main():
-    print("this is main()")
     c = config.Config()
-
-    keyValue = c.ProjectsRoot
 
     d2b = datatobackfill.DataToBackfill(c)
     projects = d2b.getProjects()
     tags = d2b.getTags()
-
-    print(projects)
-    print(tags)
-
-    dDbHelper = dynamoDbHelper.DynamoDbHelper(c)
-    dDbHelper.loadTags(tags)
-
 
     # upload pictures to S3, give read-only access, remember URLs
     s3h = s3helper.S3Helper(c)
@@ -29,13 +19,16 @@ def main():
         picturesURLs = s3h.uploadProjectPictures(project)
         project.pictures = picturesURLs
 
-    #load project tags to Dynamo
+    print(projects)
+    print(tags)
+
+    #load project Tags to Dynamo
+    #load Project details to Dynamo
     dDbHelper = dynamoDbHelper.DynamoDbHelper(c)
+    dDbHelper.loadProjects(projects)
     dDbHelper.loadTags(tags)
 
-    #load Project details to Dynamo
-
-    s = input()
+    #s = input()
 
 
 if __name__ == "__main__":
