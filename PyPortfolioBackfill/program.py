8ac45675c77093a71fd1339179e860e01f851c85
rustemvakhitov <rustemvakhitov@hotmail.com>
2025-06-11 11:13:37 -0600

    implemented uploading pictures to S3, giving read-only access, remembering URLs

--- PyPortfolioBackfill/program.py
+++ PyPortfolioBackfill/program.py
@@ -18,10 +18,16 @@ def main():
     print(projects)
     print(tags)
 
+
+    # upload pictures to S3, give read-only access, remember URLs
     s3h = s3helper.S3Helper(c)
     for project in projects:
-        s3h.uploadProjectPictures(project)
-          
+        picturesURLs = s3h.uploadProjectPictures(project)
+        project.pictures = picturesURLs
+
+    #load project tags to Dynamo
+
+    #load Project details to Dynamo
 
     s = input()
 
--- PyPortfolioBackfill/s3helper.py
+++ PyPortfolioBackfill/s3helper.py
@@ -2,6 +2,8 @@ import boto3
 from botocore.exceptions import ClientError
 import config
 import project
+import os
+import botocore
 
 
 class S3Helper():
@@ -10,14 +12,73 @@ class S3Helper():
         self.Config = config
         
 
-    def uploadProjectPictures(self, proj: project.Project):
+    def uploadProjectPictures(self, proj: project.Project) -> list[str]:
 
         self.createBucket(self.Config.BucketName, self.Config.Region)
+        self.createSubfolder(self.Config.BucketName, proj.key)
 
+        picturesURLs: list[str] = list[str]() # = list()
+
+        botoconfig = botocore.config.Config(signature_version = botocore.UNSIGNED)
+        client = boto3.client("s3", config=botoconfig)
+
+        for picture in proj.pictures:
+            self.uploadPicture(self.Config.BucketName, proj.key, picture)
+            self.setAclForPicture(self.Config.BucketName, proj.key, picture)
+
+            pictureKey = f"{proj.key}/{os.path.basename(picture)}"
+            url = client.generate_presigned_url("get_object", ExpiresIn=0, Params={"Bucket": self.Config.BucketName, "Key": pictureKey})
+            picturesURLs.append(url)
+
+        return picturesURLs
+    
+    def setAclForPicture(self, bucketName, folderName, picturePath):
+        pictureKey = f"{folderName}/{os.path.basename(picturePath)}"
+
+        self.setAclForObject(bucketName, pictureKey)
+
+        return
+    
+    def setAclForObject(self, bucketName, key):
         client = boto3.client('s3')
+
         try:
-            response = client.put_object(Bucket=self.Config.BucketName, Key=f'{proj.key}/')
+            response = client.put_object_acl(
+                Bucket=bucketName,
+                Key=key,
+                ACL='public-read'  # Set ACL to public-read
+            )
             print(response)
+            print(f"ACL updated successfully for object '{key}' in bucket '{bucketName}'.")
+        except Exception as e:
+            print(f"Error put_object_acl: {e}")
+
+        return
+    
+    def uploadPicture(self, bucketName, folderName, picturePath):
+        client = boto3.client('s3')
+        pictureKey = f"{folderName}/{os.path.basename(picturePath)}"
+
+        try:
+            response = client.upload_file(picturePath, bucketName, pictureKey)
+            print(response)
+            print(f"File '{picturePath}' uploaded to '{pictureKey}' in bucket '{bucketName}'")
+        except Exception as e:
+            print(f"Error uploading file: {e}")
+
+        return
+    
+    def createSubfolder(self, bucketName, folderName):
+
+        if self.objectExists(bucketName, folderName):
+            return
+        
+        #otherwise let's create subfolder
+        client = boto3.client('s3')
+
+        try:
+            response = client.put_object(Bucket=self.Config.BucketName, Key=f'{folderName}/')
+            #print(response)
         except ClientError as e:
             print(e)
 
@@ -31,19 +92,43 @@ class S3Helper():
         #otherwise let's create bucket
         client = boto3.client('s3')
         
-        response = client.create_bucket(
-            Bucket=bucketName,
-            CreateBucketConfiguration={
-                'LocationConstraint': region,
-            },
-        )
+        try:
+            response = client.create_bucket(
+                Bucket=bucketName,
+                ObjectOwnership='ObjectWriter',
+                CreateBucketConfiguration={
+                    'LocationConstraint': region,
+                },
+            )
+            print(response)
+
+            response = client.put_public_access_block(Bucket=bucketName, PublicAccessBlockConfiguration={'BlockPublicAcls': False,'IgnorePublicAcls': False,'BlockPublicPolicy': False,'RestrictPublicBuckets': False})
+            print(response)
+
+            response = client.put_bucket_acl(ACL='public-read',Bucket=bucketName)
+            print(response)
+        except ClientError as e:
+            print(e)
+            raise
 
         return
 
     def bucketExists(self, bucketName):
-        s3 = boto3.client('s3')
+        client = boto3.client('s3')
         try:
-            s3.head_bucket(Bucket=bucketName)
+            client.head_bucket(Bucket=bucketName)
+            return True
+        except ClientError as e:
+            if e.response['Error']['Code'] == '404':
+                return False
+            else:
+                # Handle other exceptions as needed
+                raise
+
+    def objectExists(self, bucketName, key):
+        client = boto3.client('s3')
+        try:
+            client.head_object(Bucket=bucketName, Key=key)
             return True
         except ClientError as e:
             if e.response['Error']['Code'] == '404':
